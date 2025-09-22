793bc7842a920059f4d86ee6a290477cff114e09
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-09-22 13:14:28 +0200

    Add Restock class

---
+++ src/AppForSEII2526.API/Models/Restock.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(Title), IsUnique = true)]
+    public class Restock
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DeliveryAddress {  get; set; }
+        public string Description { get; set; }
+        public DateTime ExpectedDate { get; set; }
+        public DateTime RestockDate { get; set; }
+        public decimal? TotalPrice { get; set; }
+    }
+}
