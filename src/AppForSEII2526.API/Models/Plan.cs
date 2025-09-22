23df08d0612b99db2cdb9ee7232396c4f8a13c47
José Luis Serrano <JoseLuis.Serrano4@alu.uclm.es>
2025-09-22 13:33:04 +0200

    Add Plan Class
    Added Plan class, included it in the database and created the migration

---
+++ src/AppForSEII2526.API/Models/Plan.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index (nameof(Name), IsUnique = true)]
+    public class Plan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string HealthIssues { get; set; }
+        [Precision(5,2)]
+        public decimal TotalPrice { get; set; }
+        public int Weeks { get; set; }
+
+
+    }
+}
