67c21d6c772aaf18a72789f41ee047c49611f0aa
pymas <pymas@SERGIO>
2025-09-29 19:50:03 +0200

    finish PurchaseItem class, when Item class finished, uncomment line 6

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -1,9 +1,18 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(ItemId), nameof(PurchaseId))]
     public class PurchaseItem
     {
-        public int Id { get; set; }
+        //public Item Item { get; set; }
+        public int ItemId { get; set; }
+
         public Purchase Purchase {  get; set; }
+        public int PurchaseId { get; set; }
+
+        public int AmountBought { get; set; }
+
+        [Precision(5, 2)]
+        public decimal Price { get; set; }
 
 
     }
