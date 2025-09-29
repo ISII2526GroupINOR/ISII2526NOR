882c1ebe9b791fc6598696b6891e858e56312752
Jaime3466 <Jaime.Domingo@alu.uclm.es>
2025-09-29 14:17:38 +0200

    Task3 create class restock in models (#6)
    Closes: #3
    
    * Add Restock class
    
    * Add restrictions to attributes in Restock
    
    * Add RestockItem class

---
+++ src/AppForSEII2526.API/Models/Restock.cs
@@ -0,0 +1,21 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(Title), IsUnique = true)]
+    public class Restock
+    {
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage ="Title can´t be more than 50 characters")]
+        public string Title { get; set; }
+        public string DeliveryAddress {  get; set; }
+        [StringLength(100, ErrorMessage ="Description can´t be more than 100 characters.")]
+        public string? Description { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        public DateTime ExpectedDate { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        public DateTime RestockDate { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Precision(5,2)]
+        public decimal TotalPrice { get; set; }
+        public IList<RestockItem> RestockItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/RestockItem.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class RestockItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int RestockId { get; set; }
+        [Precision(5,2)]
+        public decimal? RestockPrice { get; set; }
+        public Restock Restock { get; set; }
+    }
+}
