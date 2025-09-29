7323fef4a8f290242bd33d60a3b0d1206b2af1ef
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-09-29 17:46:21 +0200

    Task1 create class plan in models (#5)
    * Add Plan Class
    
    Added Plan class, included it in the database and created the migration
    
    * Add constraints to attributes in Plan
    
    Included constraints and other restrictions to the class Plan.
    Database also updated.
    
    * Add PlanItem
    
    Added class PlanItem and the relationships between Plan and PlanItem
    
    
    Closes:  #1

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,4 +7,7 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Restock> Restocks { get; set; }
     public DbSet<RestockItem> RestockItems { get; set; }
+
+    public DbSet<Plan> Plans { get; set; }
+    public DbSet<PlanItem> PlanItems { get; set; }
 }
