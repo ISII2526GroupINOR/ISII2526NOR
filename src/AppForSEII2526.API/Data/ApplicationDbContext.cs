289ef823a3a5c1bb6c513ed2961ef388c83c9f67
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-02 08:01:22 +0200

    Add class brand to DbContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -16,4 +16,6 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<PurchaseItem> PurchaseItems { get; set; }
 
     public DbSet<Item> Items { get; set; }
+
+    public DbSet<Brand> Brands { get; set; }
 }
