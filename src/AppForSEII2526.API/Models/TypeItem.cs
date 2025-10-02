17da388c9da7caeb535a71e1edc1a6a713e08f7e
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-02 22:21:08 +0200

    Add TypeItem attribute to Item for new relathionship. Add Items list attribute to TypeItem for new relathionship.

--- src/AppForSEII2526.API/Models/TypeItem.cs
+++ src/AppForSEII2526.API/Models/TypeItem.cs
@@ -7,5 +7,7 @@
         [StringLength(128, ErrorMessage = "TypeItem name cannot be longer than 128 characters.")]
         [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "TypeItem name can only contain letters, numbers, spaces, and hyphens.")]
         public string? Name { get; set; }
+
+        public IList<Item> Items { get; set; }
     }
 }
