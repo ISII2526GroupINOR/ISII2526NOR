b2cd9a5419b55084f224ced7b657c0ed83caf211
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-06 12:46:20 +0200

    Add not null constraint to Name in Brand. Add unique constraint to Name in Brand.

--- src/AppForSEII2526.API/Models/Brand.cs
+++ src/AppForSEII2526.API/Models/Brand.cs
@@ -1,12 +1,13 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Brand
     {
         public int Id { get; set; }
         [Required]
         [StringLength(128, ErrorMessage = "Brand name cannot be longer than 128 characters.")]
         [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "Brand name can only contain letters, numbers, spaces, and hyphens.")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public IList<Item> Items { get; set; }
     }
 }
