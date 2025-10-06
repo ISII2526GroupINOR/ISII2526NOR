4bf07887b0bad3ff9ceb13fa9f3c52c0c7b98e8f
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-06 19:22:10 +0200

    Add constraint for not nullable attribute name in class TypeItem.

--- src/AppForSEII2526.API/Models/TypeItem.cs
+++ src/AppForSEII2526.API/Models/TypeItem.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; } // Primary key
 
+        [Required]
         [StringLength(128, ErrorMessage = "TypeItem name cannot be longer than 128 characters.")]
         [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "TypeItem name can only contain letters, numbers, spaces, and hyphens.")]
-        public string? Name { get; set; }
+        public required string Name { get; set; }
 
         public IList<Item> Items { get; set; }
     }
