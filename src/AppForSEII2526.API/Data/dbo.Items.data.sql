fea89e82709d58b9b0f35389090044ce0d7ffce9
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-13 13:10:11 +0200

    Add data to script for database.

---
+++ src/AppForSEII2526.API/Data/dbo.Items.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[Items] ON
+INSERT INTO [dbo].[Items] ([Id], [Name], [Description], [PurchasePrice], [QuantityAvailableForPurchase], [QuantityForRestock], [RestockPrice], [BrandId], [TypeItemId]) VALUES (2, N'5Kg Dumbell', N'A dumbell', CAST(50.00 AS Decimal(5, 2)), 8, 2, CAST(40.00 AS Decimal(5, 2)), 1, 1)
+SET IDENTITY_INSERT [dbo].[Items] OFF
+
+SET IDENTITY_INSERT [dbo].[Brands] ON
+INSERT INTO [dbo].[Brands] ([Id], [Name]) VALUES (1, N'Adidas')
+SET IDENTITY_INSERT [dbo].[Brands] OFF
+
+SET IDENTITY_INSERT [dbo].[TypeItems] ON
+INSERT INTO [dbo].[TypeItems] ([Id], [Name], [ClassId]) VALUES (1, N'Dumbell', NULL)
+SET IDENTITY_INSERT [dbo].[TypeItems] OFF
