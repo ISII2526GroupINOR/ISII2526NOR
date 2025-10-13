1ca4324cb24e7bfa9e7886039426e7014c86fa75
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-13 12:52:56 +0200

    Create Controller for Classes
    Created the Controller file for Classes

---
+++ src/AppForSEII2526.API/Controllers/ClassesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ClassesController> _logger;
+
+        public ClassesController(ApplicationDbContext context, ILogger<ClassesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+
+            // Uses Dependency injection
+        }
+
+        // We are going to transform this method into a service of the web API.
+        // For that, it can return a Task<ActionResult> or IActionResult
+        // It is a GET
+        // It is called an action method
+        //[HttpGet]
+        //[Route("[action]")] // api/classes/ComputeDivision
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2) 
+        //{
+        //    if (op2 == 0) {
+        //        string error = "Op2 cannot be 0 to compute a division";
+        //        _logger.LogError(DateTime.Now + " Error: " + error);
+        //        return BadRequest(error);
+        //    }
+        //    decimal result = op1 / op2; 
+        //    return Ok(result);
+        //}
+    }
+}
