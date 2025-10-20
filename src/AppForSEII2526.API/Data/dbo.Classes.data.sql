7ce3d8248a70c0a109a77828d731b10f4bf45d12
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-20 11:55:38 +0200

    Add database seed for Classes

---
+++ src/AppForSEII2526.API/Data/dbo.Classes.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Classes] ON
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Date], [Capacity], [Price]) VALUES (1, N'Introduction to Boxing', N'2025-11-01 10:00:00', 35, CAST(250.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Date], [Capacity], [Price]) VALUES (2, N'Intermediate Boxing', N'2025-11-01 10:00:00', 30, CAST(400.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Date], [Capacity], [Price]) VALUES (3, N'Advanced Boxing', N'2025-11-01 10:00:00', 20, CAST(600.00 AS Decimal(5, 2)))
+SET IDENTITY_INSERT [dbo].[Classes] OFF
