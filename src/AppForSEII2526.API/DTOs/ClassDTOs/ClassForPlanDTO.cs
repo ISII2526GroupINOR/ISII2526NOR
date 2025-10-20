9a5cba106ef7012b752653d9cb1a2bbd80afd264
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-20 11:57:02 +0200

    Add DTO specification for Class objects

---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/ClassForPlanDTO.cs
@@ -0,0 +1,22 @@
+﻿
+namespace AppForSEII2526.API.DTOs.ClassDTOs
+{
+    public class ClassForPlanDTO
+    {
+        public ClassForPlanDTO(int id, string name, IList<TypeItem> types)
+        {
+            Id = id;
+            Name = name;
+            TypeItems = types;
+        }
+
+        public int Id { get; set; }
+
+        
+        [StringLength(256, ErrorMessage = "Class name cannot have more than 256 characters.")]
+        [RegularExpression(@"^[A-Za-z0-9\s]+$", ErrorMessage = "Class name can only contain alphanumeric characters and spaces.")]
+        public string Name { get; set; }
+
+        public IList<TypeItem> TypeItems { get; set; }
+    }
+}
