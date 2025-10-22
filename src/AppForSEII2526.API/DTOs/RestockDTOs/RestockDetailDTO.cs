00a87f656020f8d55fbb088783b6cedc0cdb0cdb
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-22 17:06:31 +0200

    Create DTO for getting Restock details.

---
+++ src/AppForSEII2526.API/DTOs/RestockDTOs/RestockDetailDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace AppForSEII2526.API.DTOs.RestockDTOs
+{
+    public class RestockDetailDTO
+    {
+        public RestockDetailDTO(int id, string title, string deliveryAddress, string? description, DateTime expectedDate, decimal? totalPrice, IList<RestockItem> restockItems)
+        {
+            Id = id;
+            Title = title;
+            DeliveryAddress = deliveryAddress;
+            Description = description;
+            ExpectedDate = expectedDate;
+            TotalPrice = totalPrice;
+            RestockItems = restockItems;
+        }
+
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "Title can´t be more than 50 characters")]
+        public string Title { get; set; }
+        public string DeliveryAddress { get; set; }
+        [StringLength(100, ErrorMessage = "Description can´t be more than 100 characters.")]
+        public string? Description { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        public DateTime ExpectedDate { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Precision(5, 2)]
+        public decimal? TotalPrice { get; set; }
+        public IList<RestockItem> RestockItems { get; set; }
+    }
+}
