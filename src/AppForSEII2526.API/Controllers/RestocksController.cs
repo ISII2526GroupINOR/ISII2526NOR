a42bfb9fa547e7360cbd352dd3fca58dc743f06e
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-22 16:59:54 +0200

    Create controller for Restock class. Define GetRestock method.

---
+++ src/AppForSEII2526.API/Controllers/RestocksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestocksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<RestocksController> _logger;
+
+        public RestocksController(ApplicationDbContext context, ILogger<RestocksController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(RestockDetailDTO), (int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetRestock(int id)
+        {
+
+        }
+    }
+}
