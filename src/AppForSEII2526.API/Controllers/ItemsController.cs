4391d117c2806028e5c687204287f96ed137f7e5
Sergio MG <sergio.martinez50@alu.uclm.es>
2025-10-24 08:32:12 +0200

    Resolve wrong resolve from before!

--- src/AppForSEII2526.API/Controllers/ItemsController.cs
+++ src/AppForSEII2526.API/Controllers/ItemsController.cs
@@ -11,13 +11,11 @@ namespace AppForSEII2526.API.Controllers
     {
         private ApplicationDbContext _context;
         private ILogger<ItemsController> _logger;
-
         public ItemsController(ApplicationDbContext context, ILogger<ItemsController> logger)
         {
             _context = context;
             _logger = logger;
         }
-
         //[HttpGet]
         //[Route("[action]")]
         //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
@@ -30,24 +28,34 @@ namespace AppForSEII2526.API.Controllers
         //        _logger.LogError(DateTime.Now + " Error: " + error);
         //        return BadRequest(error);
         //    }
-
         //    decimal result = op1 / op2;
         //    return Ok(result);
         //}
-
         [HttpGet]
         [Route("[action]")]
         [ProducesResponseType(typeof(IList<ItemForPurchaseDTO>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ModelError), (int)HttpStatusCode.BadRequest)]
-
-        public async Task<ActionResult> GetItemsForPurchase(string? itemName)
+        //[ProducesResponseType(typeof(ModelError), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> GetItemsForPurchase(string? itemName, string? description, string? typeItem, string? brand)
         {
             IList<ItemForPurchaseDTO> itemsDTOs = await _context
                 .Items
-                .Where(i => (i.Name.Contains(itemName) || itemName == null)  )
-                .Select(i=>new ItemForPurchaseDTO(i.Id, i.Name))
+                .Include(i => i.TypeItem)
+                .Include(i => i.PurchaseItems)
+                    .ThenInclude(pi => pi.Purchase)
+                .Where(i =>
+                    (i.Name.Contains(itemName) || itemName == null) &&
+                    (i.Description.Contains(description) || description == null) &&
+                    (i.TypeItem.Name.Equals(typeItem) || typeItem == null) &&
+                    (i.Brand.Name.Equals(brand) || brand == null) &&
+                    (i.QuantityAvailableForPurchase > 0)
+                )
+                .OrderBy(i => i.Name)
+                    .ThenBy(i => i.PurchasePrice)
+                .Select(i => new ItemForPurchaseDTO(
+                    i.Id, i.Name, i.TypeItem, i.Brand, i.Description,
+                    i.QuantityAvailableForPurchase, i.PurchasePrice
+                    ))
                 .ToListAsync();
-
             return Ok(itemsDTOs);
         }
     }
--- src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
+++ src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
@@ -8,7 +8,7 @@
         [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "Item name can only contain letters, numbers, spaces, and hyphens.")]
         public string Name { get; set; }
 
-        public ItemForPurchaseDTO(int id, string name)//, Brand brand, string description, int quantityAvailableForPurchase, decimal purchasePrice)
+        public ItemForPurchaseDTO(int id, string name, TypeItem typeItem, Brand brand, string description, int quantityAvailableForPurchase, decimal purchasePrice)
         {
             Id = id;
             Name = name;
@@ -16,16 +16,27 @@
             //Description = description;
             //QuantityAvailableForPurchase = quantityAvailableForPurchase;
             //PurchasePrice = purchasePrice;
+            TypeItem = typeItem;
+            Brand = brand;
+            Description = description;
+            QuantityAvailableForPurchase = quantityAvailableForPurchase;
+            PurchasePrice = purchasePrice;
         }
+
+        public TypeItem TypeItem { get; set; }
 
         public Brand Brand { get; set; }
         public string Description { get; set; }
 
         [Required]
         public int QuantityAvailableForPurchase { get; set; }
-
         [Required]
         [Precision(5, 2)]
         public decimal PurchasePrice { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
     }
 }
