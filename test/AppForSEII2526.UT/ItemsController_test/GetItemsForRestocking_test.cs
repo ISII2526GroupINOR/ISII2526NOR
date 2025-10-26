777e9bd79cffa59f9a57c312dab15cfbc465c11b
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-10-26 10:35:34 +0100

    Create the test method for the select method GetItemsForRestock.

---
+++ test/AppForSEII2526.UT/ItemsController_test/GetItemsForRestocking_test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.API.Controllers;
+
+namespace AppForSEII2526.UT.ItemsController_test
+{
+    internal class GetItemsForRestocking_test : AppForSEII25264SqliteUT
+    {
+    }
+}
