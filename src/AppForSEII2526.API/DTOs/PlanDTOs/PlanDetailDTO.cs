9dcccde4c55ec1358ee50313eb4301190c734d67
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-28 01:28:10 +0100

    Add PlanDetailDTO
    Implemented a DTO for encoding information regarding a specific plan.

---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PlanDetailDTO.cs
@@ -0,0 +1,61 @@
+﻿using AppForSEII2526.API.DTOs.ApplicationUserDTOs;
+using AppForSEII2526.API.DTOs.ClassDTOs;
+using DataType = System.ComponentModel.DataAnnotations.DataType; // To avoid ambiguity
+
+
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanDetailDTO
+    {
+        public PlanDetailDTO(string name, string? description, DateTime createdDate, string? healthIssues, decimal totalPrice, int weeks, ApplicationUserForPlanDetailDTO? user, IList<ClassForPlanDTO> classes)
+        {
+            Name = name;
+            Description = description;
+            CreatedDate = createdDate;
+            HealthIssues = healthIssues;
+            TotalPrice = totalPrice;
+            Weeks = weeks;
+            User = user;
+            Classes = classes;
+        }
+
+        [StringLength(128, ErrorMessage = "Plan name cannot be longer than 128 characters.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "Plan name can only contain letters, numbers, spaces, and hyphens.")]
+        public required string Name { get; set; }
+
+        [StringLength(1024, ErrorMessage = "Plan description cannot be longer than 1024 characters.")]
+        public string? Description { get; set; }
+
+        [DataType(DataType.DateTime)] // Used DataType type to take into account both date and time, as defined in the requirements.
+        public DateTime CreatedDate { get; set; } // Not null
+
+        [StringLength(4096, ErrorMessage = "Health issues cannot be longer than 4096 characters.")]
+        [Display(Name = "Health Issues")]
+        public string? HealthIssues { get; set; }
+
+        [Precision(5, 2)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; } // Not null
+
+        [Range(1, 52, ErrorMessage = "Weeks must be between 1 and 52 (1 year).")]
+        public int Weeks { get; set; } // Not null
+
+        ApplicationUserForPlanDetailDTO? User { get; set; }
+
+        IList<ClassForPlanDTO> Classes { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PlanDetailDTO dTO &&
+                   Name == dTO.Name &&
+                   Description == dTO.Description &&
+                   CreatedDate == dTO.CreatedDate &&
+                   HealthIssues == dTO.HealthIssues &&
+                   TotalPrice == dTO.TotalPrice &&
+                   Weeks == dTO.Weeks &&
+                   EqualityComparer<ApplicationUserForPlanDetailDTO?>.Default.Equals(User, dTO.User) &&
+                   EqualityComparer<IList<ClassForPlanDTO>>.Default.Equals(Classes, dTO.Classes);
+        }
+    }
+}
