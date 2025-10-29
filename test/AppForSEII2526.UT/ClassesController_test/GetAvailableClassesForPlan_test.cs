d6ccc37ca1c34086c2733496071fa0b6f97acdf5
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-29 10:20:08 +0100

    Add test class GetAvailableClassesForPlan_test

---
+++ test/AppForSEII2526.UT/ClassesController_test/GetAvailableClassesForPlan_test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForMovies.UT;
+using AppForSEII2526.API.Controllers;
+
+namespace AppForSEII2526.UT.ClassesController_test
+{
+    internal class GetAvailableClassesForPlan_test : AppForMovies4SqliteUT
+    {
+
+    }
+}
