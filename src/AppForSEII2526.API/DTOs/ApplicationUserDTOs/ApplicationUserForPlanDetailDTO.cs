d5a15e42bf2402066588e156cbeb1fe31e557e9d
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-29 22:23:21 +0100

    Task77 develop get method getplan (#79)
    closes: #77
    * Add ApplicationUserForPlanDetailDTO
    
    Implemented a DTO used for representing information regarding ApplicationUser in the context of a plan.
    
    * Add PlanDetailDTO
    
    Implemented a DTO for encoding information regarding a specific plan.
    
    * Add PlansController with GetPlanDetails method
    
    * Add data seeders for GetPlanDetails
    
    Added data seeder scripts to verify the method GetPlanDetails is working correctly.
    
    * fix: Modify accessibility of properties in PlanDetailCTO

---
+++ src/AppForSEII2526.API/DTOs/ApplicationUserDTOs/ApplicationUserForPlanDetailDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForSEII2526.API.DTOs.ApplicationUserDTOs
+{
+    /// <summary>
+    /// DTO for representing ApplicationUser details in the context of a plan.
+    /// </summary>
+    public class ApplicationUserForPlanDetailDTO
+    {
+        public ApplicationUserForPlanDetailDTO(string id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Id { get; set; }
+
+        [MaxLength(128)]
+        public string Name { get; set; }
+
+        [MaxLength(256)]
+        public string Surname { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ApplicationUserForPlanDetailDTO dTO &&
+                   Id == dTO.Id &&
+                   Name == dTO.Name &&
+                   Surname == dTO.Surname;
+        }
+    }
+}
