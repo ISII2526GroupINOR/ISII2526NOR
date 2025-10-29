34d28898d0cd38e4d4992899a4ac15b339038e86
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-29 20:51:59 +0100

    Add data seeders for GetPlanDetails
    Added data seeder scripts to verify the method GetPlanDetails is working correctly.

---
+++ src/AppForSEII2526.API/Data/dbo.PlanItems.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[PlanItems] ([PlanId], [ClassId], [Goal], [Price]) VALUES (1, 5, N'Learn boxing', CAST(250.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[PlanItems] ([PlanId], [ClassId], [Goal], [Price]) VALUES (1, 6, N'Develop boxing', CAST(400.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[PlanItems] ([PlanId], [ClassId], [Goal], [Price]) VALUES (2, 8, N'Exercise', CAST(100.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[PlanItems] ([PlanId], [ClassId], [Goal], [Price]) VALUES (2, 10, N'Exercise', CAST(100.00 AS Decimal(5, 2)))
