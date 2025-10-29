ead762e28970f0f4f7166dbc7f9bff7cc042d832
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-10-29 10:24:53 +0100

    Add reference to project resources

--- test/AppForSEII2526.UT/ClassesController_test/GetAvailableClassesForPlan_test.cs
+++ test/AppForSEII2526.UT/ClassesController_test/GetAvailableClassesForPlan_test.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppForMovies.UT;
+
 using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.ClassDTOs;
 
 namespace AppForSEII2526.UT.ClassesController_test
 {
