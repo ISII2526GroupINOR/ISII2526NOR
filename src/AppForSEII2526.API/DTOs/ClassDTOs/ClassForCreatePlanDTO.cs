e9061bb09943768088a4ddc06760a548b18f2866
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-11-04 01:10:44 +0100

    Implement partially the DTOs for PostCreatePlan

---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/ClassForCreatePlanDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.DTOs.ClassDTOs
+{
+    public class ClassForCreatePlanDTO
+    {
+        public int Id { get; set; } // Id of the selected class
+
+        public string? goal {  get; set; } // Goal of the class
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PlanForCreateDTO.cs
@@ -0,0 +1,37 @@
+﻿using AppForSEII2526.API.DTOs.ClassDTOs;
+using DataType = System.ComponentModel.DataAnnotations.DataType; // To avoid ambiguity
+
+
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanForCreateDTO
+    {
+        [Required]
+        [StringLength(128, ErrorMessage = "Plan name cannot be longer than 128 characters.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-]+$", ErrorMessage = "Plan name can only contain letters, numbers, spaces, and hyphens.")]
+        public string Name { get; set; } 
+
+        [StringLength(1024, ErrorMessage = "Plan description cannot be longer than 1024 characters.")]
+        public string? Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        [StringLength(4096, ErrorMessage = "Health issues cannot be longer than 4096 characters.")]
+        [Display(Name = "Health Issues")]
+        public string? HealthIssues { get; set; }
+
+        [Precision(5, 2)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; } // Not null
+
+        [Range(1, 52, ErrorMessage = "Weeks must be between 1 and 52 (1 year).")]
+        public int Weeks { get; set; } // Not null
+
+        [Required] // This frees the controller from null-checking
+        public IList<ClassForCreatePlanDTO> classes { get; set; }
+
+        // TODO: Missing PaymentMethod information
+    }
+}
