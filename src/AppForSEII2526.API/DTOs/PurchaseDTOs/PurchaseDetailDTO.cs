56f88c09c52ee5a3ac88e4e87105917f230722c6
Sergio MG <sergio.martinez50@alu.uclm.es>
2025-11-05 12:31:51 +0100

    fix address in PurchaseDetailDTO

--- src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
@@ -2,11 +2,14 @@
 {
     public class PurchaseDetailDTO
     {
-        public PurchaseDetailDTO(int id, string? description, string delivery_address, decimal total_price, string payment_method, List<PurchaseItem> items)
+        public PurchaseDetailDTO(int id, string? description, string street, string city, 
+            string country, decimal total_price, string payment_method, List<PurchaseItem> items)
         {
             Id = id;
             Description = description;
-            Delivery_address = delivery_address;
+            Street = street;
+            City = city;
+            Country = country;
             Total_price = total_price;
             Payment_method = payment_method;
             List<PurchaseItem> Items = items;
@@ -14,6 +17,10 @@
 
         public int Id { get; set; }
         public string? Description { get; set; } = null;
+
+        public string Street { get; set; } 
+        public string City { get; set; }
+        public string Country { get; set; }
         public string Delivery_address { get; set; }
         public decimal Total_price { get; set; }
         public string Payment_method { get; set; }
@@ -24,6 +31,9 @@
             return obj is PurchaseDetailDTO dTO &&
                    Id == dTO.Id &&
                    Description == dTO.Description &&
+                   Street == dTO.Street &&
+                   City == dTO.City &&
+                   Country == dTO.Country &&
                    Delivery_address == dTO.Delivery_address &&
                    Total_price == dTO.Total_price &&
                    Payment_method == dTO.Payment_method &&
