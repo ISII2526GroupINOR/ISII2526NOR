fd9f866abf2a5280238bea232011392b49caec3f
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-11-05 12:02:00 +0100

    Add inheritance and import neccessaries.

--- test/AppForSEII2526.UT/RestocksController_test/CreateRestock_test.cs
+++ test/AppForSEII2526.UT/RestocksController_test/CreateRestock_test.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs;
 
 namespace AppForSEII2526.UT.RestocksController_test
 {
-    internal class CreateRestock_test
+    public class CreateRestock_test : AppForSEII25264SqliteUT
     {
+        public CreateRestock_test()
+        {
+
+        }
     }
 }
