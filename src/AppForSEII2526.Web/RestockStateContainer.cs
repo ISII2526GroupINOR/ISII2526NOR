dd9b8502edb6fe8bc76d6cd574937e3e3ece61f3
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-11-11 18:16:00 +0100

    Create state container for restock.

---
+++ src/AppForSEII2526.Web/RestockStateContainer.cs
@@ -0,0 +1,43 @@
+﻿using AppForSEII2526.Web.API;
+using NuGet.Configuration;
+
+namespace AppForSEII2526.Web
+{
+    public class RestockStateContainer
+    {
+        public RestockForCreateDTO Restock { get; private set; } = new RestockForCreateDTO()
+        {
+            RestockItems = new List<ItemForCreateRestockDTO>()
+        };
+
+        public event Action? OnChange;
+
+        private void NotifyStateHasChanged() => OnChange?.Invoke();
+
+        public void AddItemToRestock(ItemForRestockingDTO item, int restockQuantity)
+        {
+            if(!Restock.RestockItems.Any(ri => ri.Id == item.Id))
+            {
+                Restock.RestockItems.Add(new ItemForCreateRestockDTO(item.Id, restockQuantity));
+            }
+        }
+
+        public void RemoveRestockItemToRestock(ItemForCreateRestockDTO item)
+        {
+            Restock.RestockItems.Remove(item);
+        }
+
+        public void ClearRestockCar()
+        {
+            Restock.RestockItems.Clear();
+        }
+
+        public void RestockProcessed()
+        {
+            Restock = new RestockForCreateDTO()
+            {
+                RestockItems = new List<ItemForCreateRestockDTO>()
+            };
+        }
+    }
+}
