80efe69461c7a7387808354aaeaab57abdd84d8b
Sergio MG <sergio.martinez50@alu.uclm.es>
2025-12-03 10:36:20 +0100

    we create the class for implementing the UIT test cases

---
+++ test/AppForSEII2526.UIT/UC_PurchaseItems/UC_PurchaseItems_UIT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_PurchaseItems
+{
+    public class UC_PurchaseItems_UIT : UC_UIT
+    {
+        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output)
+        {
+
+        }
+    }
+}
