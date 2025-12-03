bd321d26ad3a208eb7d802a90c0101e30a63dc3b
Sergio MG <sergio.martinez50@alu.uclm.es>
2025-12-03 10:57:15 +0100

    continue with PO

--- test/AppForSEII2526.UIT/UC_PurchaseItems/SelectItemsForPurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_PurchaseItems/SelectItemsForPurchase_PO.cs
@@ -9,8 +9,19 @@ namespace AppForSEII2526.UIT.UC_PurchaseItems
 {
     public class SelectItemsForPurchase_PO : PageObject
     {
+        By inputName = By.Id("inputName");
+        By inputBrand = By.Id("inputBrand");
+        By buttonSearchItems = By.Id("searchItems");
+
         public SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+
+        public void searchItems(string name)
+        {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(name);
+            _driver.FindElement(buttonSearchItems).Click();
         }
     }
 }
