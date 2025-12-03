962b3901957d50f9924a045ca01a3e8292d66fa2
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-12-03 10:36:26 +0100

    create the class for implementing the UIT test

---
+++ test/AppForSEII2526.UIT/UC_Restock/UC_RestockItems_UIT.cs
@@ -0,0 +1,16 @@
+﻿using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Restock
+{
+    public class UC_RestockItems_UIT : UC_UIT
+    {
+        public UC_RestockItems_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
