e71a197f0edd42da1ab351e39a475b8a743ad4a0
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-12-03 10:41:19 +0100

    create the page object to interact with SelectItemsForRestock

---
+++ test/AppForSEII2526.UIT/UC_Restock/SelectItemsForRestocking_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Restock
+{
+    public class SelectItemsForRestocking_PO : PageObject
+    {
+        public SelectItemsForRestocking_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
