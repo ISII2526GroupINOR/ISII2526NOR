7a60e94c85a756cb9701f8942ac72f18237af4b0
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-03 10:36:32 +0100

    Create class for implementing the UIT test cases

---
+++ test/AppForSEII2526.UIT/UC_Plan/UC_PlanClasses_UIT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class UC_PlanClasses_UIT : UC_UIT
+    {
+        public UC_PlanClasses_UIT(ITestOutputHelper output) : base(output)
+        {
+            
+        }
+    }
+}
