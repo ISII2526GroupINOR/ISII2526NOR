c37e35d0246dc85f920c582a7e94fb33b1179c67
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-12-03 10:58:08 +0100

    Add locators and iteract with the interface to search the items

--- test/AppForSEII2526.UIT/UC_Restock/SelectItemsForRestocking_PO.cs
+++ test/AppForSEII2526.UIT/UC_Restock/SelectItemsForRestocking_PO.cs
@@ -8,8 +8,20 @@ namespace AppForSEII2526.UIT.UC_Restock
 {
     public class SelectItemsForRestocking_PO : PageObject
     {
+        By inputName = By.Id("inputName");
+        By inputmin = By.Id("inputMin");
+        By inputmax = By.Id("inputMax");
+        By buttonSearchItems = By.Id("searchItems");
+
         public SelectItemsForRestocking_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+
+        public void SearchItems(string itemName)
+        {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(itemName);
+            _driver.FindElement(buttonSearchItems).Click();
         }
     }
 }
