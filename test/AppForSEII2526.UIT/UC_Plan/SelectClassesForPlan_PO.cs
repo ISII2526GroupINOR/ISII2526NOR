9286b4586f4649417dc53264ce3dd3c0be483335
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-03 10:59:49 +0100

    Add locators and interact with the interface to search the items

--- test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium.BiDi.BrowsingContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,25 @@ namespace AppForSEII2526.UIT.UC_Plan
 {
     public class SelectClassesForPlan_PO : PageObject
     {
+        By inputName = By.Id("inputName");
+        By inputDate = By.Id("inputDate");
+        By button = By.Id("searcClasses");
+
         public SelectClassesForPlan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+            
+            
+        }
+
+        public void SearchClasses(string className)
+        {
+            // Wait for the webelement to be clickable
+            WaitForBeingClickable(inputName);
+
+            _driver.FindElement(inputName).SendKeys(className);
+            _driver.FindElement(button).Click();
+            
+
         }
     }
 }
