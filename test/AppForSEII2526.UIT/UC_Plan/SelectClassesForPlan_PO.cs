36a4a43a2bfe4c8161771210bcd134f41993e481
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-03 10:41:23 +0100

    Create the PageObject to interact with SelectClassesForPlan

---
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class SelectClassesForPlan_PO : PageObject
+    {
+        public SelectClassesForPlan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
