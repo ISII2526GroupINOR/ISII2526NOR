118fc081245bccae2ac521d93b20ab5b4897719f
Jaime3466 <Jaime.Domingo@alu.uclm.es>
2025-12-08 12:37:46 +0100

    Task143 develop uit for selectitemsforrestocking   tes cases uc14 3   uc14 5 (#148)
    * update the url for running my web and update the components for testing
    
    * create the class for implementing the UIT test
    
    * create the page object to interact with SelectItemsForRestock
    
    * Add locators and iteract with the interface to search the items
    
    * introduce the filter by minimum and maximum quantity in the Page Object
    
    * Add CheckListOfItems and id to option in NavMenu
    
    * Develop test cases for the method and dbo scripts to run them
    
    * Update test methods, add new test and add dbo scripts for testing

---
+++ test/AppForSEII2526.UIT/UC_Restock/UC_RestockItems_UIT.cs
@@ -0,0 +1,73 @@
+﻿using AppForSEII2526.UIT.Shared;
+using OpenQA.Selenium.DevTools.V140.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Restock
+{
+    public class UC_RestockItems_UIT : UC_UIT
+    {
+        private SelectItemsForRestocking_PO selectItemsForRestocking_PO;
+        private const string itemName1 = "Resistance Band";
+        private const string itemBrand1 = "Rogue Fitness";
+        private const string itemStockQuantity1 = "8";
+        private const string itemRestockPrice1 = "7";
+
+        private const string itemName2 = "20 Kg Kettlebell";
+        private const string itemBrand2 = "Precor";
+        private const string itemStockQuantity2 = "2";
+        private const string itemRestockPrice2 = "40";
+
+        public UC_RestockItems_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectItemsForRestocking_PO = new SelectItemsForRestocking_PO(_driver, _output);
+        }
+
+        private void Precondition_perform_login()
+        {
+            Perform_login("jaime@uclm.es", "12$Temporary");
+        }
+
+        private void InitialStepsForRestockItems()
+        {
+            Precondition_perform_login();
+            selectItemsForRestocking_PO.WaitForBeingVisible(By.Id("CreateRestock"));
+            _driver.FindElement(By.Id("CreateRestock")).Click();
+        }
+
+        [Theory]
+        [InlineData(itemName1, itemBrand1, itemStockQuantity1, itemRestockPrice1, "Resistance Band", "", "")]
+        [InlineData(itemName2, itemBrand2, itemStockQuantity2, itemRestockPrice2, "", "", "2")]
+        [InlineData(itemName1, itemBrand1, itemStockQuantity1, itemRestockPrice1, "", "8", "")]
+        [Trait("LevelTesting", "Functional Testing")]
+        public void UC14_3_AF2_UC14_2_3_4_filtering(string itemName, string itemBrand, string itemStockQuantity, string itemRestockPrice,
+            string filterName, string filterMin, string filterMax)
+        {
+            // Arrange
+            InitialStepsForRestockItems();
+            var expectedItems = new List<string[]> { new string[] { itemName, itemBrand, itemStockQuantity, itemRestockPrice } };
+
+            // Act
+            selectItemsForRestocking_PO.SearchItems(filterName, filterMin, filterMax);
+
+            // Assert
+            Assert.True(selectItemsForRestocking_PO.CheckListOfItems(expectedItems));
+        }
+
+        //[Fact(Skip = "first run dbo.Items.data.UpdateQuantityAvailable.sql, after run dbo.Items.data.RecoverQuantityAvailable")]
+        [Fact]
+        [Trait("LevelTesting", "Functional Testing")]
+        public void UC14_AF1_UC14_13_filtering()
+        {
+            InitialStepsForRestockItems();
+            var expectedItems = new List<string[]> { };
+
+            selectItemsForRestocking_PO.SearchItems("", "", "");
+
+            Assert.True(selectItemsForRestocking_PO.CheckListOfItems(expectedItems));
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_Restock/dbo.Items.data.UpdateQuantityAvailable.sql
@@ -0,0 +1,4 @@
+﻿Update [dbo].[Items] set [QuantiyAvailableForPurchase] = 8 where Id = 100
+Update [dbo].[Items] set [QuantiyAvailableForPurchase] = 6 where Id = 100
+Update [dbo].[Items] set [QuantiyAvailableForPurchase] = 4 where Id = 100
+Update [dbo].[Items] set [QuantiyAvailableForPurchase] = 2 where Id = 100
