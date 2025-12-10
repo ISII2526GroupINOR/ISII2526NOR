24cdb9f082de7b60ede65824ea1753bf05696903
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-10 23:28:53 +0100

    Add login precondition
    Note that the fact that credentials are in plain text within version control is intended

--- test/AppForSEII2526.UIT/UC_Plan/UC_PlanClasses_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Plan/UC_PlanClasses_UIT.cs
@@ -9,9 +9,17 @@ namespace AppForSEII2526.UIT.UC_Plan
 {
     public class UC_PlanClasses_UIT : UC_UIT
     {
+
+
         public UC_PlanClasses_UIT(ITestOutputHelper output) : base(output)
         {
             
+        }
+
+
+        private void Precondition_perform_login()
+        {
+            Perform_login("user2@email.lan", "Pass1234$"); // Plain text credentials in version control are intended for testing purposes.
         }
 
     }
