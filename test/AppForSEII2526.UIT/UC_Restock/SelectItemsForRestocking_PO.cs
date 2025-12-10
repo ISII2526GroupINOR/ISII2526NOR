1a4804de211ac9cb1831586201e815b483928363
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2025-12-10 13:54:26 +0100

    Create check of error

--- test/AppForSEII2526.UIT/UC_Restock/SelectItemsForRestocking_PO.cs
+++ test/AppForSEII2526.UIT/UC_Restock/SelectItemsForRestocking_PO.cs
@@ -13,6 +13,7 @@ namespace AppForSEII2526.UIT.UC_Restock
         By inputmax = By.Id("inputMax");
         By buttonSearchItems = By.Id("searchItems");
         By tableOfItemsBy = By.Id("TableOfItems");
+        By errorShownBy = By.Id("ErrorShown");
 
         public SelectItemsForRestocking_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
@@ -35,6 +36,13 @@ namespace AppForSEII2526.UIT.UC_Restock
         public bool CheckListOfItems(List<string[]> expectedItems)
         {
             return CheckBodyTable(expectedItems, tableOfItemsBy);
+        }
+
+        public bool CheckMessageError(string message)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual message shown: {actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(message);
         }
     }
 }
