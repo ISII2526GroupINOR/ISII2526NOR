28c74382fac28d12dc010a374d86c315a7b9f103
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-11 01:46:55 +0100

    Add test cases UC1ES1P1, UC1ES3P1 and UC1ES3P2

--- test/AppForSEII2526.UIT/UC_Plan/UC_PlanClasses_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Plan/UC_PlanClasses_UIT.cs
@@ -15,24 +15,24 @@ namespace AppForSEII2526.UIT.UC_Plan
         // Class 1
         private const int classId1 = 1;
         private const string className1 = "Judo";
-        private readonly DateOnly classDate1 = TimeTable.nextWeekMonday;
-        private readonly TimeOnly classTime1 = TimeTable.timeMorning;
+        private static readonly DateOnly classDate1 = TimeTable.nextWeekMonday;
+        private static readonly TimeOnly classTime1 = TimeTable.timeMorning;
         private const decimal classPrice1 = 250.00m;
-        private readonly List<string> classTypeItems1 = new List<string> {"Mat"};
+        private static readonly List<string> classTypeItems1 = new List<string> { "Mat" };
         // Class 2
         private const int classId2 = 2;
         private const string className2 = "Boxing";
-        private readonly DateOnly classDate2 = TimeTable.middleOfNextWeek;
-        private readonly TimeOnly classTime2 = TimeTable.timeAfternoon;
+        private static readonly DateOnly classDate2 = TimeTable.middleOfNextWeek;
+        private static readonly TimeOnly classTime2 = TimeTable.timeAfternoon;
         private const decimal classPrice2 = 400.00m;
-        private readonly List<string> classTypeItems2 = new List<string> {"Gloves", "Punching Bag"};
+        private static readonly List<string> classTypeItems2 = new List<string> { "Gloves", "Punching Bag" };
         // Class 3
         private const int classId3 = 3;
         private const string className3 = "Yoga";
-        private readonly DateOnly classDate3 = TimeTable.nextWeekSunday;
-        private readonly TimeOnly classTime3 = TimeTable.timeEvening;
+        private static readonly DateOnly classDate3 = TimeTable.nextWeekSunday;
+        private static readonly TimeOnly classTime3 = TimeTable.timeEvening;
         private const decimal classPrice3 = 100.00m;
-        private readonly List<string> classTypeItems3 = new List<string> {"Machine"};
+        private static readonly List<string> classTypeItems3 = new List<string> { "Machine" };
 
 
         public UC_PlanClasses_UIT(ITestOutputHelper output) : base(output)
@@ -55,29 +55,53 @@ namespace AppForSEII2526.UIT.UC_Plan
             selectClassesForPlan_PO.WaitForBeingVisible(By.Id("CreatePlan"));
 
             // Click on the option of the menu to go to the plan creation page
-            _driver.FindElement(By.Id("CreatePlan")).Click();   
+            _driver.FindElement(By.Id("CreatePlan")).Click();
         }
 
 
-        [Fact]
+
+
+        [Theory]
         [Trait("LevelTesting", "Functional Testing")]
-        public void UC1_ES1_1_createAPlan()
+        [MemberData(nameof(TestCasesFor_UC1ES1P1_and_UC1ES3P1_and_UC1ES3P2))]
+        public void UC1ES1P1_and_UC1ES3P1_and_UC1ES3P2(string classNameFilter, DateOnly? classDateFilter, List<string[]> expectedClasses)
         {
             // ARRANGE
             InitialStepsForPlanClasses();
+            // ACT
+            selectClassesForPlan_PO.SearchClasses(classNameFilter, classDateFilter);
+            // ASSERT
+            Assert.True(selectClassesForPlan_PO.CheckListOfClasses(expectedClasses));
+        }
 
-            var expectedClasses = new List<string[]> // Use default decimal places for prices
+        public static IEnumerable<object[]> TestCasesFor_UC1ES1P1_and_UC1ES3P1_and_UC1ES3P2()
+        {
+            // Prepare expected classes data for test cases UC1_ES1_1 and UC1_ES3_1 to UC1_ES3_2
+            var expectedClasses_UC1ES1P1 = new List<string[]> // No filters
             {
                 new string[] { className1, classDate1.ToString("dd/MM/yyyy"), classTime1.ToString("HH:mm"), classPrice1.ToString("F2", CultureInfo.InvariantCulture), string.Join(", ", classTypeItems1), "Add" },
                 new string[] { className2, classDate2.ToString("dd/MM/yyyy"), classTime2.ToString("HH:mm"), classPrice2.ToString("F2", CultureInfo.InvariantCulture), string.Join(", ", classTypeItems2), "Add" },
                 new string[] { className3, classDate3.ToString("dd/MM/yyyy"), classTime3.ToString("HH:mm"), classPrice3.ToString("F2", CultureInfo.InvariantCulture), string.Join(", ", classTypeItems3), "Add" }
             };
+            var expectedClasses_UC1ES3P1 = new List<string[]> // Filter "Ju"
+            {
+                new string[] { className1, classDate1.ToString("dd/MM/yyyy"), classTime1.ToString("HH:mm"), classPrice1.ToString("F2", CultureInfo.InvariantCulture), string.Join(", ", classTypeItems1), "Add" },
+            };
+            var expectedClasses_UC1ES3P2 = new List<string[]> //Filter by date 
+            {
+                new string[] { className2, classDate2.ToString("dd/MM/yyyy"), classTime2.ToString("HH:mm"), classPrice2.ToString("F2", CultureInfo.InvariantCulture), string.Join(", ", classTypeItems2), "Add" },
+            };
 
-            // ACT
-            selectClassesForPlan_PO.SearchClasses("", null); // No filters
+            // Prepare test cases
+            var testCases = new List<object[]>
+            {
+                new object[] { "", null, expectedClasses_UC1ES1P1 }, // UC1_ES1_1: No filters
+                new object[] { "Ju", null, expectedClasses_UC1ES3P1 }, // UC1_ES3_1: Filter by name "Ju"
+                new object[] { "", TimeTable.middleOfNextWeek, expectedClasses_UC1ES3P2 } // UC1_ES3_2: Filter by date classDate2
+            };
 
-            // ASSERT
-            Assert.True(selectClassesForPlan_PO.CheckListOfClasses(expectedClasses));
+
+            return testCases;
         }
     }
 }
