5c4c872af7205af8c6cd233879d05db7f7164446
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-13 01:06:50 +0100

    Add CreatePlan_PO for testing the plan creation UI

---
+++ test/AppForSEII2526.UIT/UC_Plan/CreatePlan_PO.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.BiDi.BrowsingContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class CreatePlan_PO : PageObject
+    {
+
+        public CreatePlan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+
+        }
+    }
+}
