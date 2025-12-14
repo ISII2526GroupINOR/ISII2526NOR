252d3d20b1bc9ea6fb3b698285d254e2abd75507
JL6502 <145500929+JL6502@users.noreply.github.com>
2025-12-14 01:35:06 +0100

    Added auxiliary methods in CreatePlan_PO to support UIT for plan details

--- test/AppForSEII2526.UIT/UC_Plan/CreatePlan_PO.cs
+++ test/AppForSEII2526.UIT/UC_Plan/CreatePlan_PO.cs
@@ -48,5 +48,47 @@ namespace AppForSEII2526.UIT.UC_Plan
             WaitForBeingClickable(buttonModifyClasses);
             _driver.FindElement(buttonModifyClasses).Click();
         }
+
+        public void setPlanName(string name)
+        {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).Clear();
+            _driver.FindElement(inputName).SendKeys(name);
+        }
+
+        public void setPlanDescription(string description)
+        {
+            WaitForBeingClickable(inputDescription);
+            _driver.FindElement(inputDescription).Clear();
+            _driver.FindElement(inputDescription).SendKeys(description);
+        }
+
+        public void setPlanWeeks(int weeks)
+        {
+            WaitForBeingClickable(inputWeeks);
+            _driver.FindElement(inputWeeks).Clear();
+            _driver.FindElement(inputWeeks).SendKeys(weeks.ToString());
+        }
+
+        public void setPlanHealthIssues(string healthIssues)
+        {
+            WaitForBeingClickable(inputHealthIssues);
+            _driver.FindElement(inputHealthIssues).Clear();
+            _driver.FindElement(inputHealthIssues).SendKeys(healthIssues);
+        }
+
+        // TODO: implement method to set the payment method when it is available
+
+        public void SubmitPlan()
+        {
+            WaitForBeingClickable(buttonSubmit);
+            _driver.FindElement(buttonSubmit).Click();
+        }
+
+        public string GetErrors()
+        {
+            WaitForBeingVisible(errorPanel);
+            return _driver.FindElement(errorPanel).Text;
+        }
     }
 }
