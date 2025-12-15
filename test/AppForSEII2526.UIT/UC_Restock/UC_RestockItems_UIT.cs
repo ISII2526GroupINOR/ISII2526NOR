a80a91055085a1ac5f5dfe630daf15b408418f6f
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-12-15 13:30:26 +0100

    UPdate dates

--- test/AppForSEII2526.UIT/UC_Restock/UC_RestockItems_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Restock/UC_RestockItems_UIT.cs
@@ -97,7 +97,7 @@ namespace AppForSEII2526.UIT.UC_Restock
             string title = "A title1";
             string address = "any";
             string description = "Restock for me";
-            string date = "13/12/2025";
+            string date = "13/12/2028";
             int quantity = 10;
 
             InitialStepsForRestockItems();
@@ -126,9 +126,9 @@ namespace AppForSEII2526.UIT.UC_Restock
         [Theory]
         [InlineData("A title2", "Any", "", "1/12/2025", 10, "Error! The expected date must start later than today")]
         [InlineData("A title3", "Any", "Wrong description", "13/12/2025", 10, "Error! The description must start with: Restock for")]
-        [InlineData("A title4", "Any", "", "13/12/2025", 1, "Error! The total quantity for purchase 2 plus the quantity to " +
+        [InlineData("A title4", "Any", "", "13/12/2028", 1, "Error! The total quantity for purchase 2 plus the quantity to " +
             "restock 1 of item 20 Kg Kettlebell must be bigger than the quantity for restock 5.")]
-        [InlineData("A title4", "Any", "", "13/12/2025", 0, "You need to restock at least one item.")]
+        [InlineData("A title4", "Any", "", "13/12/2028", 0, "You need to restock at least one item.")]
         [Trait("LevelTesting", "Functional Testing")]
         public void UC14_AF4_UC14_8_9_10_13(string title, string address, string description,
             string date, int quantity, string errorMessage)
