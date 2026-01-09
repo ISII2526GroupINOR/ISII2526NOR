817731fa34ff662568356cc187c370b50ee911bb
JAIME DOMINGO FERNÁNDEZ <Jaime.Domingo@alu.uclm.es>
2026-01-09 16:40:54 +0100

    Add agent responses

--- src/AppForSEII2526.Maui/Services/AssistantServices.cs
+++ src/AppForSEII2526.Maui/Services/AssistantServices.cs
@@ -40,7 +40,7 @@ namespace AppForSEII2526.Maui.Services
             if (message == null || message == "") return message;
             input = input.ToLower();
 
-            if (Regex.IsMatch(input, @"(language|idiom)"))
+            if (Regex.IsMatch(input, @"(language|idiom|entiendo|idioma)"))
             {
                 if (context == "/adminconfiguration")
                 {
@@ -54,6 +54,48 @@ namespace AppForSEII2526.Maui.Services
                     else if (role == "user") dest = "/userprofile";
                 }
             }
+            else if (Regex.IsMatch(input, @"(font|letter|letters)"))
+            {
+                if (context == "/adminconfiguration")
+                {
+                    message = "You are just there, just click the second box, below your user image.";
+                }
+                else
+                {
+                    message = "Want to change those letters, huh? Just go to configuration, in your profile" +
+                        ", there you can change the font to a better one.";
+                    if (role == "admin") dest = "/adminconfiguration";
+                    else if (role == "user") dest = "/userprofile";
+                }
+            }
+            else if (Regex.IsMatch(input, @"(widget|widgets)"))
+            {
+                if (context == "/adminconfiguration")
+                {
+                    message = "You are just there, just click the tick boxes below your user image enable and disable them.";
+                }
+                else
+                {
+                    message = "Don´t like those widgets, huh? Just go to configuration, in your profile" +
+                        ", there you can hide and show them.";
+                    if (role == "admin") dest = "/adminconfiguration";
+                    else if (role == "user") dest = "/userprofile";
+                }
+            }
+            else if (Regex.IsMatch(input, @"(restock|items|item)"))
+            {
+                if (context == "/adminhome" && Preferences.Get("wRestock", true))
+                {
+                    message = "You can press the first widget, the one saying restock. If you want to restock a concrete item," +
+                        "you can press it, and it will be automatically selected.";
+                }
+                else if (role=="admin")
+                {
+                    message = "You can press the second item from the left of the nav menu below. That will take you there.";
+                    if (role == "admin") dest = "/adminhome";
+                }
+            }
+
             return message;
         }
 
@@ -62,6 +104,64 @@ namespace AppForSEII2526.Maui.Services
         {
             string message = "No se que quieres bro.";
             dest = "";
+            if (message == null || message == "") return message;
+            input = input.ToLower();
+
+            if (Regex.IsMatch(input, @"(language|idiom|entiendo|idioma)"))
+            {
+                if (context == "/adminconfiguration")
+                {
+                    message = "Casi lo tienes, solo pulsa la primera caja de texto, debajo de tu imagen de perfil.";
+                }
+                else
+                {
+                    message = "No entiendes nada, eh? Solo ve a configuración, en tu perfil" +
+                        ", ahí puedes cambiar la imagen de perfil.";
+                    if (role == "admin") dest = "/adminconfiguration";
+                    else if (role == "user") dest = "/userprofile";
+                }
+            }
+            else if (Regex.IsMatch(input, @"(fuente|letra|letras)"))
+            {
+                if (context == "/adminconfiguration")
+                {
+                    message = "Casi lo tienes, solo pulsa la segunda caja de texto, debajo de tu imagen de perfil.";
+                }
+                else
+                {
+                    message = "¿Quieres cambiar esas letras, eh? Solo ve a la configuración, en tu perfil" +
+                        ", ahí puedes cambiar el tipo de letra.";
+                    if (role == "admin") dest = "/adminconfiguration";
+                    else if (role == "user") dest = "/userprofile";
+                }
+            }
+            else if (Regex.IsMatch(input, @"(widget|widgets)"))
+            {
+                if (context == "/adminconfiguration")
+                {
+                    message = "Ya estas ahí, solo pulsa las cajas con ticks para deshabilitar y habilitar los widgets.";
+                }
+                else
+                {
+                    message = "No te gustan esos widgets? Ve a configuración, en tu perfil" +
+                        ", ahí puedes ocultarlos y mostrarlos.";
+                    if (role == "admin") dest = "/adminconfiguration";
+                    else if (role == "user") dest = "/userprofile";
+                }
+            }
+            else if (Regex.IsMatch(input, @"(restock|stock|item|abastecer|artículo|articulo)"))
+            {
+                if (context == "/adminhome" && Preferences.Get("wRestock", true))
+                {
+                    message = "Puedes pulsar el primer widget y te llevará a reabastecer" +
+                        ". También puedes pulsar el artículo que quieres reabastecer y te lo seleccionará automáticamente.";
+                }
+                else if (role == "admin")
+                {
+                    message = "Pulsa el segundo icono del menú inferior. Te llevará a la ventana de reabastecimiento ahí.";
+                    if (role == "admin") dest = "/adminhome";
+                }
+            }
 
             return message;
         }
